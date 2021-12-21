78e8a7378dace6943712c363cbd60c3c5982aaa5
Deepanshu Rautela <deepanshu.mb20@gmail.com>
2021-12-21 17:16:13 +0530

    Change Deepanshu component

--- src/Components/Deepanshu.jsx
+++ src/Components/Deepanshu.jsx
@@ -1,7 +1,23 @@
 const DeepanshuComponent = () => {
+    //a list of random white names
+    const whiteNames = [
+        "Aaliyah",
+        "Aaron",
+        "Abagail",
+        "Abbey",
+        "Abbie",
+        "Abbigail",
+        "Abby",
+        "Abdiel",
+        "Abdul",
+    ];
     return (
         <div>
-            <h1>Deepanshu</h1>
+            <ul>
+            {
+                whiteNames.map(name => <li>{name}</li>)
+            }
+            </ul>
         </div>
     );
 }
